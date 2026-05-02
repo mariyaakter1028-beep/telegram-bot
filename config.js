61a40aba9195ccaf14640c814cc7741b4643154d
mariyaakter1028-beep <mariyaakter1028@gmail.com>
2026-05-02 23:33:48 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const BOT_TOKEN = "8529411138:AAEBvQ-4h9zEa9_zaRbB4FP8qktF6lZqeJc";
+export const BOT_TOKEN = "8529411138:AAGZli5a-0CPz3vcs6Hce1mS5pa_DFilzCE";
 export const CHAT_IDS = ["-1003527248014"];
