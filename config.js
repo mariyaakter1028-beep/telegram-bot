3a0c5646311f0620fb5116a38d333198db7be1a6
mariyaakter1028-beep <mariyaakter1028@gmail.com>
2026-05-02 23:27:58 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,2 @@
-export const USERNAME = 'Shorfuddin904@gmail.com';
-export const PASSWORD = 'Onetimeuse';
-export const BOT_TOKEN = '8529411138:AAHXcvnEaawCeefYXnrBU2B7ferI21jpG5A';
-export const INLINE_LINK = 'https://t.me/Global_Method_Channel';
-export const CODE_RESET_TIME = 120;
-export const CHAT_IDS = ['-1003527248014'];
-export const REFRESH_INTERVAL_MINUTES = 60;
-export const ALL_NUMBER_URL = 'https://www.orangecarrier.com/live/calls';
+export const BOT_TOKEN = "8529411138:AAEBvQ-4h9zEa9_zaRbB4FP8qktF6lZqeJc";
+export const CHAT_IDS = ["-1003527248014"];
